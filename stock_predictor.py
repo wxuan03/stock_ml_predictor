62a1a2aab7e0838c2115f6715cd1d59b4733b70b
William Xuan <xuanwilliam8@gmail.com>
2023-12-21 16:58:06 -0800

    basic graph of ticker

--- stock_predictor.py
+++ stock_predictor.py
@@ -1,1 +1,13 @@
-print("First Commit Test")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+import yfinance as yf
+
+ticker = 'GOOG'
+data = yf.download(ticker, start='2010-01-01', end='2023-01-01')
+data.Close.plot(title=f"{ticker} Stock Price")
+plt.show()
+
